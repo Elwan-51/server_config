9834a959e380929a1e0997ae6d4d75c02bd8fc0e
Elwan LEFEVRE <elwan.lefevre@asylium.fr>
2024-12-19 15:41:33 +0000

    UPDATE : add_env.sh

--- script/add_env.sh
+++ script/add_env.sh
@@ -7,8 +7,9 @@ do
 
 declare -u APP=$arg
 
+echo "# DNS Settings for $arg" >> $path_env
 echo "TYPE_$APP=\""$arg"\"" >> $path_env
 echo "URI_$APP=\"asylium.app\"" >> $path_env
 echo "TARGET_$APP=\"CNAME\"" >> $path_env
-
+echo " ">> $path_env
 done
