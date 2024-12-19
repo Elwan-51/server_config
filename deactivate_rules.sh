9cb93605368093b1a98869e2ae666cabfd8fb26a
Ubuntu <ubuntu@ns3907642.ip-54-38-44.eu>
2024-12-19 07:53:09 +0000

    init

---
+++ deactivate_rules.sh
@@ -0,0 +1,12 @@
+#! /bin/bash
+
+file_extention=".yaml"
+unactivated_dir="unactivated"
+host_dir="asylium1"
+docker_path="/home/ubuntu/docker"
+if [ -n "$2" ]; then
+host_dir=$2
+fi
+
+
+cp $docker_path/appdata/traefik/rules/$host_dir/$1$file_extention $docker_path/appdata/traefik/rules/$unactivated_dir/ && rm $docker_path/appdata/traefik/rules/$host_dir/$1$file_extention
